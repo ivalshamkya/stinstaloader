2582909ade1ba1027dc30846c811a268f9efb88a
Ival Shamkya <ivalshamkya@gmail.com>
2023-11-24 17:51:35 +0800

    feat: edit download function

--- download_functions.py
+++ download_functions.py
@@ -7,14 +7,14 @@ import re
 
 my_username = "hydenjkyl"
 L = instaloader.Instaloader()
-LLLL = instaloader.Instaloader()
 L.context.log("Login...")
 L.load_session_from_file(my_username, "instaloader.session")
 # Initialization
 
 def download_all_posts(username):
     try:
-        profile = instaloader.Profile.from_username(LLLL.context, username)
+        loader = instaloader.Instaloader()
+        profile = instaloader.Profile.from_username(loader.context, username)
         image_urls = [post.url for post in profile.get_posts()]
         print(f"All posts from {username} URLs obtained successfully.")
         return image_urls
@@ -52,7 +52,8 @@ def download_post(post_link):
     if match:
         mediaid = match.group(1)
         try:
-            post = instaloader.Post.from_shortcode(LLLL.context, mediaid)
+            loader = instaloader.Instaloader()
+            post = instaloader.Post.from_shortcode(loader.context, mediaid)
             full_image_url = post.graph_target.shortcode_media.image_versions2.candidates[0].url
             image_urls = [post.url]
             print(image_urls)
@@ -73,7 +74,8 @@ def download_all_post_slides(post_link, quality="low"):
     if match:
         mediaid = match.group(1)
         try:
-            post = instaloader.Post.from_shortcode(LLLL.context, mediaid)
+            loader = instaloader.Instaloader()
+            post = instaloader.Post.from_shortcode(loader.context, mediaid)
             pictures = [node.display_url + f"?quality={quality}" for node in post.get_sidecar_nodes()]
             print(f"All slides from {post.owner_username} URL obtained successfully.")
             return pictures, post.caption
