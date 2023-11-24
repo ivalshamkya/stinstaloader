5d6f30fd32054f960a224258f2319ebd0838892c
Ival Shamkya <ivalshamkya@gmail.com>
2023-11-25 00:22:48 +0800

    feat: add progress bar

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,18 +1,19 @@
 import streamlit as st
 from PIL import Image
-from io import BytesIO
 import requests
+from io import BytesIO
 import os
+import time
 from dotenv import load_dotenv
 
 load_dotenv()
 
 def run_app():
-    st.title("Instagram Downloader")
-
+    title = "Instagram Downloader"
+    title_el = st.empty()
     hide_st_style = """
     <style>
-    #MainMenu {visibility: hidden;}
+    MainMenu {visibility: hidden;}
     footer {visibility: hidden;}
     # header {visibility: hidden;}
     </style>
@@ -20,18 +21,33 @@ def run_app():
     st.markdown(hide_st_style, unsafe_allow_html=True)
 
     # Menu for selecting download type
-    download_type = st.sidebar.selectbox("Select Download Type", ["Post & Reels", "Story", "All Posts"])
-
+    download_type = st.sidebar.selectbox("Select Download Type", ["Post & Reels", "Story - Currently not available", "All Posts"])
+    button_submit_text = "Download"
     # Input field for Instagram link or username
-    if download_type in ["All Posts", "Story"]:
-        username = st.text_input("Enter Instagram username:")
+    title += " " + download_type
+    input_field = st.empty()
+    if download_type in ["All Posts", "Story - Currently not available"]:
+        if download_type == "Story - Currently not available":
+            title = "Not Available"
+            username = input_field.text_input("Enter Instagram username:", value="", disabled=True)
+        else:
+            username = input_field.text_input("Enter Instagram username:", value="", placeholder="Username")
     else:
-        link = st.text_input("Enter Instagram link:")
+        link = input_field.text_input("Enter Instagram link:", value="", placeholder="https://www.instagram.com/p/example")
 
-    if st.button("Download"):
+    title_el.title(title)
+
+    submit_button = st.button("Run", use_container_width=False)
+
+    if submit_button:
         try:
+            progress_bar = st.empty()
+            progress_bar.progress(0, text="Please wait(0%)...")
+
+            dividerEl = st.empty()
             result = None
-            base_url =  os.getenv('API_URL') # Update with your Flask API URL
+            
+            base_url = os.getenv('API_URL')  # Update with your Flask API URL
 
             if download_type == "All Posts" and username:
                 endpoint = "/download_all_posts"
@@ -54,26 +70,34 @@ def run_app():
                 result = requests.get(f"{base_url}{endpoint}", params=params).json()
 
             if result is not None:
+                dividerEl.divider()
+                total_progress = 100
+                increment = total_progress // len(result)
+                current_progress = 0
                 if len(result) == 2:
                     for url in result[0]:
-                        print("aa")
                         if url["is_video"]:
                             st.video(BytesIO(requests.get(url["url"]).content))
                         else:
                             image = Image.open(BytesIO(requests.get(url["url"]).content))
                             st.image(image, use_column_width=True)
+                        current_progress += increment
+                        progress_bar.progress(current_progress, text=f"Please wait({current_progress}%)...")
                     st.text(result[1])
                 else:
-                    print(result)
                     for url in result:
-                        print(url)
                         if url["is_video"]:
                             st.video(BytesIO(requests.get(url["url"]).content))
                         else:
                             image = Image.open(BytesIO(requests.get(url["url"]).content))
                             st.image(image, use_column_width=True)
+                        current_progress += increment
+                        progress_bar.progress(current_progress, text=f"Please wait({current_progress}%)...")
+                progress_bar.progress(100, text=f"Finished({100}%).")
                 st.success("Download successful!")
             else:
+                progress_bar.progress(0, text="error occurred while downloading")
                 st.error("Download failed. Please check the link or username.")
         except requests.exceptions.RequestException as e:
             st.error(f"Request error: {e}")
+
